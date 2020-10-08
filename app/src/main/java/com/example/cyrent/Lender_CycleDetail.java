3646d856fac330055c5720f57717b18a35750f59
Arshdeep Singh <beawaredof@gmail.com>
2020-10-09 00:40:39 +0530

    animation added

--- app/src/main/java/com/example/cyrent/Lender_CycleDetail.java
+++ app/src/main/java/com/example/cyrent/Lender_CycleDetail.java
@@ -3,12 +3,30 @@ package com.example.cyrent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Lender_CycleDetail extends AppCompatActivity {
-
+    boolean isCheckDone = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lender__cycle_detail);
+        final LottieAnimationView lottieCheckDone=findViewById(R.id.lottieCheck1);
+        lottieCheckDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isCheckDone){
+                    lottieCheckDone.setSpeed(-3);
+                    lottieCheckDone.playAnimation();
+                    isCheckDone=false;
+                }else{
+                    lottieCheckDone.setSpeed(3);
+                    lottieCheckDone.playAnimation();
+                    isCheckDone=true;
+                }
+            }
+        });
     }
 }
