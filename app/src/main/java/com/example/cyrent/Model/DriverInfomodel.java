52d54d48dda00fd8b5e828bcde79994f45d56ffe
Arshdeep Singh <beawaredof@gmail.com>
2020-10-12 18:20:01 +0530

    login changed

---
+++ app/src/main/java/com/example/cyrent/Model/DriverInfomodel.java
@@ -0,0 +1,38 @@
+package com.example.cyrent.Model;
+
+public class DriverInfomodel {
+   private String first_Name,last_Name,PhoneNumber;
+   private double rating;
+
+    public String getFirst_Name() {
+        return first_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        this.first_Name = first_Name;
+    }
+
+    public String getLast_Name() {
+        return last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        this.last_Name = last_Name;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
