8ca0ea985b4208d72ddc68974f4cdd99cf0a5e20
Shray Tyagi <st.shray200130@gmail.com>
2020-10-12 23:39:31 +0530

    Sign Out working

--- app/src/main/java/com/example/cyrent/Common.java
+++ app/src/main/java/com/example/cyrent/Common.java
@@ -3,4 +3,16 @@ package com.example.cyrent;
 public class Common {
     public static final String DRIVER_INFO_REFERENCE="Driver info";
     public static final String DRIVER_LOCATION_REFERENCES ="LenderLocation" ;
+
+    /*
+      public static String buildWelcomeMessage() {
+        if(Common.currentUser != null)
+        {
+            return new StringBuilder("Welcome ")
+                    .append(Common.currentUser.getFirstName())
+                    .append(" ")
+                    .append(Common.currentUser.getLastName());
+        }
+        else return "";
+    } */
 }
--- app/src/main/java/com/example/cyrent/LenderHomeActivity.java
+++ app/src/main/java/com/example/cyrent/LenderHomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.cyrent;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,9 +24,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import org.w3c.dom.Text;
+
 public class LenderHomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +40,76 @@ public class LenderHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lender_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.nav_sign_out)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LenderHomeActivity.this);
+                    builder.setTitle("Sign Out")
+                    .setMessage("Do you really want to Sign Out?")
+                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).setPositiveButton("SIGN OUT", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            FirebaseAuth.getInstance().signOut();
+                            Intent intent = new Intent(LenderHomeActivity.this, SplashScreen.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }).setCancelable(false);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.setOnShowListener(dialog1 -> {
+                        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                                .setTextColor(getResources().getColor(android.R.color.holo_red_dark, null ));
+                        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                                .setTextColor(getResources().getColor(R.color.colorAccent, null));
+
+                    });
+
+                        dialog.show();
+
+
+                }
+
+                return true;
+            }
+        });
+
+        //Set Data for user
+        View headerView = navigationView.getHeaderView(0);
+        TextView txt_name = (TextView)headerView.findViewById(R.id.txt_name);
+        TextView txt_phone = (TextView)headerView.findViewById(R.id.txt_phone);
+      //  TextView txt_star = (TextView)headerView.findViewById(R.id.txt_name);
+
+     //   txt_name.setText(Common.buildWelcomeMessage());
+      //  txt_phone.setText(Common.currentUser!=null ? Common.currentUser.getPhoneNumber() : "");
+
+
+
+
+
     }
 
     @Override
